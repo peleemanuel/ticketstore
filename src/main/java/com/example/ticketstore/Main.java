139cfb2144b24ed8648006860c23898f02050e0b
George Pop <george@George>
2023-05-16 23:27:55 +0300

    TICS-1 Added Stage title and icon

--- src/main/java/com/example/ticketstore/Main.java
+++ src/main/java/com/example/ticketstore/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,13 +14,21 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+            Image icon = new Image("file:src/main/resources/com/example/ticketstore/icons/person_icon.png"); // daca vreau sa mearga poza,
+
+            stage.getIcons().add(icon);
+
             Scene scene = new Scene(root);
             stage.setScene(scene);
+            stage.setTitle("Login");
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) { launch(); }
+    public static void main(String[] args) {
+        launch();
+    }
 }
